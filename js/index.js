b686ae2306e663372b85dd29dfd199a8a9c800a4
ZJing <izengjing.cn@gmail.com>
2017-04-08 22:44:35 +0800

    zjing's personal web

--- js/index.js
+++ js/index.js
@@ -2,11 +2,15 @@
  * Created by ZJing on 2017/4/7.
  */
 require(['jquery'],function(){
-    var $subMenu = $(".sub-menu");
-    var $menu = $("#menu a");
-    $menu.hover(function(){
-        $subMenu.show();
-    },function(){
-        $subMenu.hide();
-    });
+    // var $subMenu = $(".sub-menu");
+    // var $menu = $("#menu a");
+    // $menu.hover(function(){
+    //     $subMenu.show();
+    // },function(){
+    //     $subMenu.hide();
+    // });
+    var $header = $("#header");
+    if(window.OffsetHeight < 0){
+        $header.style.position = 'fixed';
+    }
 })
