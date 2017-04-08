7e5e433f5e265e5f3eb199fab6c74b36b484d5b3
ZJing <izengjing.cn@gmail.com>
2017-04-08 20:38:01 +0800

    zjing's personal web

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ZJing on 2017/4/7.
+ */
+require(['jquery'],function(){
+    var $subMenu = $(".sub-menu");
+    var $menu = $("#menu a");
+    $menu.hover(function(){
+        $subMenu.show();
+    },function(){
+        $subMenu.hide();
+    });
+})
